39825603fe3af2f06acec276e5d62658e285212c
Mark Landeryou <samurai96@hotmail.com>
2022-02-28 11:41:44 -0500

    refactor: removed an unneeded p from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Square to Round Converter</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
